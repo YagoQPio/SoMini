e57ba8201adcca64c3c23c639c687a8efaf5e862
YagoQPio <126572216+YagoQPio@users.noreply.github.com>
2024-11-13 20:19:57 -0300

    Update permissoes.go

--- permissoes.go
+++ permissoes.go
@@ -8,17 +8,20 @@ type FileMeta struct {
 	Owner string
 }
 
+// Armazena permissões para cada arquivo/diretório
 var permissions = make(map[string]FileMeta)
 
+// Define o proprietário de um arquivo
 func setFileOwner(filePath string, owner string) {
 	permissions[filePath] = FileMeta{Owner: owner}
 }
 
-func checkPermissions(user *User, filePath string) bool {
+// Verifica se o usuário tem permissão para acessar um arquivo
+func checkPermissions(usuario *Usuario, filePath string) bool {
 	fileMeta, exists := permissions[filePath]
 	if !exists {
 		fmt.Println("Arquivo ou diretório não encontrado.")
 		return false
 	}
-	return fileMeta.Owner == user.Username
+	return fileMeta.Owner == usuario.Username // Alterado de usuario.Nome para usuario.Username
 }
